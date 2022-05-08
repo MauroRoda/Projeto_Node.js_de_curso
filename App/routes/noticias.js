9e8121b75a3666cf1a08de646efaf19ff5b2a1fa
Mauro Roda <Mauro@Mauro-PC.(mauro)>
2022-05-08 19:34:44 -0300

    atualização

--- App/routes/noticias.js
+++ App/routes/noticias.js
@@ -1,6 +1,24 @@
 module.exports = function(app){
-    app.get('/noticias', function(req,res){
-        res.render("noticias/noticias");
-    } );
 
+    /* Inicio modulo para conecção dom o banco de dados */
+    
+        app.get('/noticias', function(req, res){
+    
+        var mysql = require('mysql');
+        var connection = mysql.createConnection({
+            host :'localhost',
+            user : 'root',
+            password : 'Mauro@123',
+            database : 'projeto2'
+        });
+    
+        /* executando consulta dentro do mysql */
+            connection.query('select *  from noticias', function(erro, result){
+            /* tratativa do retorno dado após consulta do banco */
+                    res.send(result);
+
+        });
+
+        });
+    
 };
