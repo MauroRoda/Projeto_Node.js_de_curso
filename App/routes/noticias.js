58c36a0c9605477d62a96692fd558b485327d8a6
Mauro Roda <Mauro@Mauro-PC.(mauro)>
2022-05-09 18:28:36 -0300

    atualização

--- App/routes/noticias.js
+++ App/routes/noticias.js
@@ -15,10 +15,10 @@ module.exports = function(app){
         /* executando consulta dentro do mysql */
             connection.query('select *  from noticias', function(erro, result){
             /* tratativa do retorno dado após consulta do banco */
-                    res.send(result);
+                   // res.send(result);
+                   res.render("noticias/noticias", {noticias : result});
 
-        });
-
+            });
         });
     
 };
